1504c22b9f1ca633f46e6f6fc54641f3e3192609
JasonGROSSO <114397870+JasonGROSSO@users.noreply.github.com>
2022-11-10 12:28:19 +0100

    request + form

--- app.js
+++ app.js
@@ -3,32 +3,48 @@ import sunCalc from "./sunCalc.js";
 sunCalc.getTimes(/*Date*/ date, /*Number*/ latitude, /*Number*/ longitude, /*Number (default=0)*/ height)
 
 function getSaddress() {
-    let input =document.getElementById("saddress").value;
+    let input = document.getElementById("saddress").value;
     return input
 }
 function getCity() {
-    let input =document.getElementById("city").value;
+    let input = document.getElementById("city").value;
     return input
 }
 function getCountry() {
-    let input =document.getElementById("country").value;
+    let input = document.getElementById("country").value;
+    return input
+}
+function getRegion() {
+    let input = document.getElementById("region").value;
+    return input
+}
+function getZIPCode() {
+    let input = document.getElementById("zip").value;
     return input
 }
 var CountDownDate = new Date("date").getTime();
-var x = setInterval(function(){
+var x = setInterval(function () {
     var now = new Date().getTime();
     var distance = CountDownDate - now;
 
-    var days = Math.floor(distance / (1000 * 60 * 60 *24))
-    var hours = Math.floor((distance % (1000 * 60 * 60 *24)) / (1000 * 60 * 60))
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24))
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
     var secondes = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById("Santa_Countdown").innerHTML = days + "d" + hours + "h" + minutes + "m" + secondes + "s" ;
+    document.getElementById("Santa_Countdown").innerHTML = days + "d" + hours + "h" + minutes + "m" + secondes + "s";
 
-    if (distance < 0 ) {
+    if (distance < 0) {
         clearInterval(x);
         document.getElementById("timer").innerHTML = "Santa arrived"
     }
 }, 1000);
-new Request("https://nominatim.openstreetmap.org/search.php?street=STREET&city=CITY&region=REGION&country=COUNTRY&format=jsonv2")
+function OnClick() {
+    let street = getSaddress
+    let city = getCity
+    let region = getRegion
+    let country = getCountry
+    let ZIPcode = getZIPCode
+    new Request(`https://nominatim.openstreetmap.org/search.php?street=${street}&city=${city}&region=${region}&country=${country}&ZIPcode=${ZIPcode}&format=jsonv2`)
+
+}
