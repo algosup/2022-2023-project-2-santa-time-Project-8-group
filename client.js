c67321d899e3b974a797c8d9c423d2aef26d2b7c
JasonGROSSO <114397870+JasonGROSSO@users.noreply.github.com>
2022-11-30 12:16:20 +0100

    countdown auto update
    TODO clearInterval

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
+var timer;
 async function submit() {
-    // TODO: Get the form data
     const form = document.getElementById('form');
     let street = form.street;
     let city = form.city;
@@ -11,17 +11,18 @@ async function submit() {
         let timestamp = json["time"];
         let date = new Date(timestamp);
 
-        // Calculate time difference
-        let now = Date.now();
-        let remaining = new Date(timestamp - now); // Using a date object to simplify time components extraction
-        let remainingDays = remaining.getUTCMonth() * 12 + remaining.getUTCDate() - 1;
-        let remainingHours = remaining.getUTCHours();
-        let remainingMinutes = remaining.getUTCMinutes();
-        let remainingSeconds = remaining.getUTCSeconds();
-        // Note: -1 because since this is a date object, the day of the month starts at 1 and not 0
-        document.getElementById('days').innerHTML = remainingDays;
-        document.getElementById('hours').innerHTML = remainingHours;
-        document.getElementById('minutes').innerHTML = remainingMinutes;
-        document.getElementById('seconds').innerHTML = remainingSeconds;
+            var timer = setInterval(function() {
+                let now = Date.now();
+                let remaining = new Date(timestamp - now);
+                let remainingDays = remaining.getUTCMonth() * 12 + remaining.getUTCDate() - 1;
+                let remainingHours = remaining.getUTCHours();
+                let remainingMinutes = remaining.getUTCMinutes();
+                let remainingSeconds = remaining.getUTCSeconds();
+
+                document.getElementById('days').innerHTML = remainingDays;
+                document.getElementById('hours').innerHTML = remainingHours;
+                document.getElementById('minutes').innerHTML = remainingMinutes;
+                document.getElementById('seconds').innerHTML = remainingSeconds;
+                }, 1000); 
     });
 }
