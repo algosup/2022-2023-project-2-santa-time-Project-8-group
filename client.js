449c1d617c82d1e75764b3a88fc69129a9d0910f
JasonGROSSO <114397870+JasonGROSSO@users.noreply.github.com>
2022-12-01 14:16:40 +0100

    update

--- client.js
+++ client.js
@@ -10,24 +10,21 @@ async function submit() {
     response.json().then((json) => {
         let timestamp = json["time"];
         let date = Date.now(timestamp);
-            if (timer != "undefined") {
-                var timer = setInterval(function() {
+            if (timer == undefined) {
+                clearInterval(timer);
+                };
+            timer = setInterval(function() {
                     let now = Date.now();
                     let remaining = new Date(timestamp - now);
                     let remainingDays = remaining.getUTCMonth() * 12 + remaining.getUTCDate() - 1;
                     let remainingHours = remaining.getUTCHours();
                     let remainingMinutes = remaining.getUTCMinutes();
                     let remainingSeconds = remaining.getUTCSeconds();
-    
                     document.getElementById('days').innerHTML = remainingDays;
                     document.getElementById('hours').innerHTML = remainingHours;
                     document.getElementById('minutes').innerHTML = remainingMinutes;
                     document.getElementById('seconds').innerHTML = remainingSeconds;
                     }, 1000); 
-            }
-            else {
-                clearInterval(timer)
-            };
             
     });
 }
