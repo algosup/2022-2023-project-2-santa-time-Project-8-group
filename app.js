0308b7394ae5f3b040942cdb8421f8a84662e75f
leo-chartier <leo.chartier@algosup.com>
2022-12-05 15:14:10 +0100

    add sharing buttons and other links
    Share on:
    - Twitter
    - Facebook
    - LinkedIn
    - Reddit
    - Whatsapp
    - Copy to clipboard
    
    Links:
    - ALGOSUP
    - Github
    - Contact address

--- app.js
+++ app.js
@@ -16,6 +16,22 @@ app.get("/", async (req, res) => {
     }
 });
 
+// TODO: Create folders and refactor code to serve
+// static files more easily with express
+// (Exercise for Jason)
+/*
+    public/
+        css/
+            stylesheet.css
+        images/
+            ...
+        js/
+            client.js
+        website.html
+    private/
+        app.js
+        modules.js
+*/
 app.get("/stylesheet.css", (req, res) => {
    res.write(fs.readFileSync(__dirname + "/stylesheet.css", 'utf8'));
    res.end();
@@ -23,6 +39,30 @@ app.get("/stylesheet.css", (req, res) => {
 app.get("/client.js", (req, res) => {
    res.write(fs.readFileSync(__dirname + "/client.js", 'utf8'));
    res.end();
+});
+app.get("/images/twitter.svg", (req, res) => {
+    res.sendFile(__dirname + "/images/twitter.svg")
+});
+app.get("/images/facebook.png", (req, res) => {
+    res.sendFile(__dirname + "/images/facebook.png")
+});
+app.get("/images/linkedin.png", (req, res) => {
+    res.sendFile(__dirname + "/images/linkedin.png")
+});
+app.get("/images/reddit.png", (req, res) => {
+    res.sendFile(__dirname + "/images/reddit.png")
+});
+app.get("/images/whatsapp.svg", (req, res) => {
+    res.sendFile(__dirname + "/images/whatsapp.svg")
+});
+app.get("/images/copy.svg", (req, res) => {
+    res.sendFile(__dirname + "/images/copy.svg")
+});
+app.get("/images/github.png", (req, res) => {
+    res.sendFile(__dirname + "/images/github.png")
+});
+app.get("/images/algosup.png", (req, res) => {
+    res.sendFile(__dirname + "/images/algosup.png")
 });
 
 app.listen(port);
