5063260efb3039356bd43fbf3233b830cce4baa3
Chaput-Mathieu <mathieu.chaput@algosup.com>
2022-12-08 17:28:12 +0100

    add modification for https in Dockerfile and app and credentials in forlder santa...

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ const path = require("path");
 const fs = require("fs");
 const express = require("express");
 const modules = require("./modules.js");
+const https = require("https");
+const http = require("http");
+
+var options = {
+    key: fs.readFileSync("santa.algosup.com/privkey.pem"),
+    cert: fs.readFileSync("santa.algosup.com/cert.pem")
+};
+
 
 const app = express();
-const port = process.env.PORT || 80;
+// const port = process.env.PORT || 80;
+const port = 443;
 
 app.get("/", async (req, res) => {
     let params = modules.GetParameters(req.url);
@@ -25,4 +34,14 @@ app.get("/client.js", (req, res) => {
    res.end();
 });
 
-app.listen(port);
+//redirect all traffic to https
+app.use(function(req, res, next) {
+    if (req.secure) {
+        next();
+    } else {
+        res.redirect('https://' + req.headers.host + req.url);
+    }
+});
+//listen to http requests
+http.createServer(app).listen(80);
+https.createServer(options,app).listen(port);
