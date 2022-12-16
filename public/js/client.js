bdc206a9a7cb99f042893d994b7411bc7555841c
Chaput-Mathieu <mathieu.chaput@algosup.com>
2022-12-16 09:24:24 +0100

    add change

--- public/js/client.js
+++ public/js/client.js
@@ -16,14 +16,24 @@ async function submit() {
     let address = form.address.defaultValue;
     let response = await fetch(`/?address=${address}`);
     response.json().then((json) => {
-        // console.log(json)
         document.getElementsByClassName("presents")[0].classList.remove("show");
         let timestamp = json["time"];
+        let country = "";
+        let street = "";
+        let region = "";
+        document.forms.address.disabled = false;
+        document.forms.address.value = "";
+        let sanitzedAddress = stringSanitizer(document.forms.address.defaultValue); //make the address without accents to compare with the country, region and street
+        let sanitzedCountry = stringSanitizer(json["country"]); //make the country without accents to compare with the address
+        let sanitzedRegion = stringSanitizer(json["region"]); //make the region without accents to compare with the address
+        let sanitzedStreet = stringSanitizer(json["street"]); //make the street without accents to compare with the address
+        if (sanitzedAddress.search(new RegExp(sanitzedCountry,"i")) == -1) country = ", " + json["country"];
+        if (sanitzedAddress.search(new RegExp(sanitzedRegion,"i")) == -1) region = ", " + json["region"];
+        if (sanitzedAddress.search(new RegExp(sanitzedStreet,"i")) == -1) street = ", " + json["street"];
         if (timestamp == undefined) {
             document.forms.address.placeholder = "Address " + document.forms.address.defaultValue + " not found";
             return;
         }
-        let date = Date.now(timestamp);
         if (timer != undefined) {
             clearInterval(timer);
         };
@@ -40,8 +50,12 @@ async function submit() {
             document.getElementById('seconds').innerHTML = remainingSeconds;
             document.getElementsByClassName("presents")[0].classList.add("show");
         }, 100);
-        document.forms.address.value = json["country"] + ", " + document.forms.address.defaultValue;
-        document.forms.address.disabled = false;
+        document.forms.address.value =  document.forms.address.defaultValue + street + region + country;
         document.forms.address.style += ";font-size: 2vh;"
     });
 };
+
+function stringSanitizer(string) {
+    if (typeof string !== 'string') return '';
+    return string.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/-/g," ");
+}
